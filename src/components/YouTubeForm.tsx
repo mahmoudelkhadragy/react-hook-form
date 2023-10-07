b836f45022479e04d418dec21d261ed07b922bf9
mahmoudelkhadragy <mahmoudelkhadragy38@gmail.com>
2023-10-08 00:33:14 +0300

    add start project with setup

---
+++ src/components/YouTubeForm.tsx
@@ -0,0 +1,95 @@
+import { useForm } from "react-hook-form";
+import { DevTool } from "@hookform/devtools";
+// set form cvalues type
+type FormValues = {
+  username: string;
+  email: string;
+  channel: string;
+};
+export const YouTubeForm = () => {
+  // make default value after fetch data
+  /*
+  const form = useForm<FormValues>({
+    defaultValues: async () => {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users/1"
+      );
+      const data = await response.json();
+      return {
+        username: "Batman",
+        email: data.email,
+        channel: "",
+      };
+    },
+  });
+  */
+  const form = useForm<FormValues>({
+    defaultValues: {
+      username: "Batman",
+      email: "",
+      channel: "",
+    },
+  });
+  // object return from useForm has this vlaues
+  const { register, control, handleSubmit, formState } = form;
+  const { errors } = formState;
+  console.log(formState);
+  console.log(control);
+  console.log(register);
+
+  const onSubmit = (data: FormValues) => {
+    console.log(data);
+  };
+  // noValidate => to override html validation and set your pure validations
+  return (
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)} noValidate>
+        <div className="form-contol">
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            {...register("username", {
+              required: "Username is required",
+            })}
+          />
+          <p className="error ">{errors.username?.message}</p>
+        </div>
+        <div className="form-control">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            {...register("email", {
+              pattern: {
+                value:
+                  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                message: "Invalid email format",
+              },
+              validate: (val) => {
+                return (
+                  val !== "admin@example.com" ||
+                  "Enter a different email address"
+                );
+              },
+            })}
+          />
+          <p className="error">{errors.email?.message}</p>
+        </div>
+        <div className="form-control">
+          <label htmlFor="channel">Channel</label>
+          <input
+            type="text"
+            id="channel"
+            {...register("channel", {
+              required: "Channel is required",
+            })}
+          />
+          <p className="error">{errors.channel?.message}</p>
+        </div>
+        <button>Submit</button>
+      </form>
+      <DevTool control={control} />
+    </div>
+  );
+};
