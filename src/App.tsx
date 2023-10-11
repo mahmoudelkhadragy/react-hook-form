0d8bad9006bc85ff5c22cd5a7bd8a9deb947e23f
mahmoudelkhadragy <mahmoudelkhadragy38@gmail.com>
2023-10-11 12:00:09 +0300

    mui integrate with react hook form

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import "./App.css";
+// import "./App.css";
+import { LoginForm } from "./components/MuiLoginForm";
 // import { YouTubeForm } from "./components/YouTubeForm";
 // import { YupYouTubeForm } from "./components/YupYouTubeForm";
-import { ZodYouTubeForm } from "./components/ZodYouTubeForm";
+// import { ZodYouTubeForm } from "./components/ZodYouTubeForm";
 function App() {
   return (
     <>
       {/* <YouTubeForm /> */}
       {/* <YupYouTubeForm /> */}
-      <ZodYouTubeForm />
+      {/* <ZodYouTubeForm /> */}
+      <LoginForm />
     </>
   );
 }
